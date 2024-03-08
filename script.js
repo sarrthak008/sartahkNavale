bb2eb0e0fa7f570aa81e82e725a983801dece372
sarrthak008 <sarthaknavale08@gmail.com>
2024-03-08 13:14:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let menu=document.querySelector("#mleft")
+menu.addEventListener('click',()=>{
+    hambPage=document.querySelector('#hamb');
+    hambPage.style.left=0+'vw'
+    
+document.addEventListener("scroll",(e)=>{
+    hambPage.style.left=-100+'vw'
+    hambPage.style.transition='all 0.1s ease-in-out'
+ })
+})
+
+let closeMenu=document.querySelector('#cross')
+closeMenu.addEventListener('click',()=>{
+    hambPage=document.querySelector('#hamb')
+    hambPage.style.left=-100+'vw'
+})
+
+
+
+var video = document.getElementById('player');
+video.addEventListener('click',function(){
+    if(video.paused){
+        video().play();
+    }else{
+        video.pause();
+    }
+},false);
+
+
+document.addEventListener("contextmenu",(e)=>{
+e.preventDefault();
+})
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
